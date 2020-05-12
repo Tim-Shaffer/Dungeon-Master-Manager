d2bf856f3c41a7cd420c50b2cb78105a6f19dcb9
Tim-Shaffer <tcshaf24@ptd.net>
2020-05-11 23:29:17 -0400

    trying new way for register/login and have register route working

---
+++ validation/register.js
@@ -0,0 +1,38 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  // Convert empty fields to an empty string so we can use validator functions
+  data.username  = !isEmpty(data.username) ? data.username : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+  // Username  checks
+  if (Validator.isEmpty(data.username)) {
+    errors.name = "Username field is required";
+  }
+
+  // Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
