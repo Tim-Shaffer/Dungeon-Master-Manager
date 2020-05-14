b359981f8aa99f6f97e9d799d1e84d31d8ab0094
Tim-Shaffer <tcshaf24@ptd.net>
2020-05-14 12:15:22 -0400

    updated player view for multiple characters

--- client/src/components/Playerview/Playerview.js
+++ client/src/components/Playerview/Playerview.js
@@ -6,6 +6,8 @@ class Playerview extends Component {
 
     state = {
         characters: [
+            {name: "Test Character 2", attributes: [{attrName: "Class", attrValue: 1},]},
+            {name: "Test Character 3", attributes: [{attrName: "Class", attrValue: 1},]},
             {name: "Test Character 1",
             attributes: [
                 {attrName: "Class", attrValue: 1}, 
@@ -14,6 +16,7 @@ class Playerview extends Component {
                 {attrName: "Race", attrValue: 4} 
             ]
         }]
+        // characters: []
     };
 
     render() {
@@ -22,17 +25,57 @@ class Playerview extends Component {
         <div>
             <div className="container-fluid" id="body">
                 <div className="row">
-                    <div className="col-6" id="campaign">
+                    {/* <div className="col-6" id="campaign">
                         <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Create Character</button>
                     </div>
                     <div className="col-6" id="campaign">
                         <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Characters</button>
-                    </div>
+                    </div> */}
                 </div>
                 <br/>
                 <br/>
                 <br/>
-                <div className="row justify-content-center" id="card">
+                {this.state.characters.length > 0 ?
+                    this.state.characters.length === 1 ?
+                    <div className="row justify-content-center" id="card">
+                        <div className="col-4">
+                            {this.state.characters.length > 0 ?
+                            <div className="card border border-dark">
+                                
+                                {this.state.characters.map(character => <PlayerCard character={character.name} attributes={character.attributes}></PlayerCard>) }
+                                                
+                                <div className="butt">
+                                    <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Edit</a> 
+                                    <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Remove</a>
+                                </div>
+                            </div>
+                            :
+                            <div className="col-12" id="campaign">
+                                <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Create Character</button>
+                            </div>
+                            } 
+                        </div>
+                    </div>
+                    :
+                    <div className="row" id="card">
+                        {this.state.characters.map(character => 
+                        <div className={this.state.characters.length/4 === 1 ? "col-4" : "col-3"}>
+                            <div className="card border border-dark">
+                                <PlayerCard character={character.name} attributes={character.attributes}></PlayerCard>                
+                                <div className="butt">
+                                    {/* <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Edit</a> */}
+                                    <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Remove</a>
+                                </div>
+                            </div>
+                        </div>)} 
+                    </div>
+                    
+                :
+                <div className="col-12" id="campaign">
+                    <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Create Character</button>
+                </div>
+                }
+                {/* <div className="row justify-content-center" id="card">
                     <div className="col-4">
                         {this.state.characters.length > 0 ?
                         <div className="card border border-dark">
@@ -40,15 +83,17 @@ class Playerview extends Component {
                             {this.state.characters.map(character => <PlayerCard character={character.name} attributes={character.attributes}></PlayerCard>) }
                                              
                             <div className="butt">
-                                <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Edit</a>
+                                <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Edit</a> 
                                 <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Remove</a>
                             </div>
                         </div>
                         :
-                        <div>Add a new character</div>
+                        <div className="col-12" id="campaign">
+                        <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Create Character</button>
+                    </div>
                         } 
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>   
         </>
