32211f7ce0d6b880748dde2b9beda206535d9a1f
Tim-Shaffer <tcshaf24@ptd.net>
2020-05-13 22:43:23 -0400

    updates to react components to allow more flexibility for both the DM and Player views

---
+++ client/src/components/PlayerCard/Playercard.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { List, ListItem } from "../List";
+import "./style.css";
+
+class PlayerCard extends Component {
+
+  render() {
+    return (
+      <>
+      {/* {this.state.players.length > 0 ? */}
+        <div className="card-header"><h5 className="card-title">{this.props.character}</h5></div>
+        <div className="card-body">
+          {this.props.attributes.length > 0 ?
+            <List>
+              {this.props.attributes.map(attribute =>
+              <ListItem >{attribute.attrName}:  <span className="attr-value">{attribute.attrValue}</span> </ListItem>
+              )}
+              {/* // <ListItem ><span className="attr-name">Class:  </span><span className="attr-value">1</span></ListItem>
+              // <ListItem ><span className="attr-name">Level:  </span><span className="attr-value">2</span></ListItem>
+              // <ListItem ><span className="attr-name">Background:  </span><span className="attr-value">3</span></ListItem>
+              // <ListItem ><span className="attr-name">Race:  </span><span className="attr-value">4</span></ListItem>
+              // <ListItem ><span className="attr-name">Alignment:  </span><span className="attr-value">5</span></ListItem>
+              // <ListItem ><span className="attr-name">Experience:  </span><span className="attr-value">6</span></ListItem>
+              // <ListItem ><span className="attr-name">Strength:  </span><span className="attr-value">7</span></ListItem>
+              // <ListItem ><span className="attr-name">Dexterity:  </span><span className="attr-value">8</span></ListItem>
+              // <ListItem ><span className="attr-name">Constitution:  </span><span className="attr-value">9</span></ListItem>
+              // <ListItem ><span className="attr-name">Intelligence:  </span><span className="attr-value">10</span></ListItem>
+              // <ListItem ><span className="attr-name">Wisdom:  </span><span className="attr-value">11</span></ListItem>
+              // <ListItem ><span className="attr-name">Charisma:  </span><span className="attr-value">12</span></ListItem> */}
+            </List>
+            :
+            <div>No Attributes</div>
+            }
+        </div>
+      </>
+    );
+  }
+
+}
+
+export default PlayerCard;
--- client/src/components/Playerview/Playerview.js
+++ client/src/components/Playerview/Playerview.js
@@ -1,55 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import "./playerstyle.css";
+import PlayerCard from "../PlayerCard/Playercard";
 
-function Playerview() {
+class Playerview extends Component {
 
-  return (
-    <body>
+    state = {
+        characters: [
+            {name: "Test Character 1",
+            attributes: [
+                {attrName: "Class", attrValue: 1}, 
+                {attrName: "Level", attrValue: 2}, 
+                {attrName: "Background", attrValue: 3}, 
+                {attrName: "Race", attrValue: 4} 
+            ]
+        }]
+    };
 
-    <div>
-        <div className="container-fluid" id="body">
-            <div className="row">
-                <div className="col-6" id="campaign">
-                    <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Create Character</button>
+    render() {
+        return (
+        <>
+        <div>
+            <div className="container-fluid" id="body">
+                <div className="row">
+                    <div className="col-6" id="campaign">
+                        <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Create Character</button>
+                    </div>
+                    <div className="col-6" id="campaign">
+                        <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Characters</button>
+                    </div>
                 </div>
-                <div className="col-6" id="campaign">
-                    <button type="button" className="btn btn-danger btn-lg playerbttn border border-dark">Characters</button>
-                </div>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <div className="row justify-content-center" id="card">
-                <div className="col-4">
-                    <div className="card border border-dark">
-                        <div className="card-header">
-                        Player 1
+                <br/>
+                <br/>
+                <br/>
+                <div className="row justify-content-center" id="card">
+                    <div className="col-4">
+                        {this.state.characters.length > 0 ?
+                        <div className="card border border-dark">
+                            
+                            {this.state.characters.map(character => <PlayerCard character={character.name} attributes={character.attributes}></PlayerCard>) }
+                                             
+                            <div className="butt">
+                                <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Edit</a>
+                                <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Remove</a>
+                            </div>
                         </div>
-                        <div className="card-body">
-                        <h5 className="card-title">Name of player</h5>
-                        <p className="card-text">class & Level:</p>
-                        <p className="card-text">Background:</p>
-                        <p className="card-text">Race:</p>
-                        <p className="card-text">Alignment:</p>
-                        <p className="card-text">Experience:</p>
-                        <p className="card-text">Strength:</p>
-                        <p className="card-text">Dexterity:</p>
-                        <p className="card-text">Constitution:</p>
-                        <p className="card-text">Intelligence:</p>
-                        <p className="card-text">Wisdom:</p>
-                        <p className="card-text">Charisma:</p>
-                        </div>                    
-                        <div className="butt">
-                            <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Edit</a>
-                            <a href="#" className="btn btn-primary btn-block playerbttn border border-dark" id="playerbttn">Remove</a>
-                        </div>
+                        :
+                        <div>Add a new character</div>
+                        } 
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    </body>
-  );
+        </div>   
+        </>
+        );
+    }
+
 }
 
 export default Playerview;
