4686c80bb7d2fb53ca6d3fd9db6894a076ddfbbe
Leopold Acosta <leopoldacosta@Leopolds-iMac.local>
2020-05-17 22:28:56 -0400

    added info jumbotron to landing page providing page information

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import NavbarLanding from "../Navbar/NavbarLanding";
 import Jumbo from "../Jumbotron/Jumbotron";
+import Info from "../Info/Info";
 
 class Landing extends Component {
   render() {
     return (
       <>
         <NavbarLanding />
+        <Info />
         {/* <Jumbo /> */}
       </>
     );
